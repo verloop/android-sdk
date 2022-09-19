0c289a9cf6a87af8847e750df246c72b72bac59f
Prashant Kedia <prashantkedia22@gmail.com>
2022-09-19 16:35:14 +0530

    Fixed - Push notifications on API 33

--- sdk/src/main/java/io/verloop/sdk/ui/VerloopActivity.kt
+++ sdk/src/main/java/io/verloop/sdk/ui/VerloopActivity.kt
@@ -1,13 +1,13 @@
 package io.verloop.sdk.ui
 
+import android.Manifest
 import android.content.Intent
 import android.graphics.Color
-import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import android.view.MenuItem
-import android.view.WindowInsets
-import android.view.WindowManager
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.core.content.ContextCompat
@@ -75,6 +75,7 @@ class VerloopActivity : AppCompatActivity() {
             viewModel?.getClientInfo()?.observe(this) { clientInfo -> updateClientInfo(clientInfo) }
             addFragment()
         }
+        requestPermissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS)
     }
 
     override fun onResume() {
@@ -122,5 +123,15 @@ class VerloopActivity : AppCompatActivity() {
         super.onActivityResult(requestCode, resultCode, data)
         Log.d(TAG, "onActivityResult")
         verloopFragment.fileUploadResult(requestCode, resultCode, data)
+    }
+
+    private val requestPermissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ) { isGranted: Boolean ->
+        if (isGranted) {
+            // Permission is granted. Continue the action or workflow in your app.
+        } else {
+            Toast.makeText(this, "Notifications Disabled", Toast.LENGTH_SHORT).show()
+        }
     }
 }
