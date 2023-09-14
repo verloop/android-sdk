e173d806114009982e2af68922691d2a1ccf9884
androiddev <droiddevverloop@gmail.com>
2023-09-14 11:31:46 +0530

    Fixed typo

--- sdk/src/main/java/io/verloop/sdk/Constants.kt
+++ sdk/src/main/java/io/verloop/sdk/Constants.kt
@@ -6,6 +6,6 @@ object Constants {
     internal const val JS_CALL_SET_DEPARTMENT = "JS_CALL: SetDepartment"
     internal const val JS_CALL_SET_RECIPE = "JS_CALL: SetRecipe"
     internal const val JS_CALL_SET_CUSTOM_FIELD = "JS_CALL: SetCustomField"
-    internal const val JS_CALL_SET_WIDGET_OPENED = " JS_CALL:WidgetOpened"
+    internal const val JS_CALL_SET_WIDGET_OPENED = "JS_CALL: WidgetOpened"
     internal const val JS_CALL_SET_CLOSE = "JS_CALL: Close"
 }
