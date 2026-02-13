f67f910246a1425beafcd0f401865607711c1ed2
Subhadip Mondal <159517480+subhadipverloop@users.noreply.github.com>
2026-02-13 18:43:30 +0530

    removed: webview debug

--- app/src/main/java/io/verloop/TestActivity.kt
+++ app/src/main/java/io/verloop/TestActivity.kt
@@ -30,7 +30,6 @@ class TestActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_test)
-        WebView.setWebContentsDebuggingEnabled(true);
 
         // Use either header config or overrideHeaderLayout
         headerConfig = HeaderConfig.Builder()
